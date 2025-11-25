3b738940e04e79ac1ce88cb13b3095c9e6d992e5
Gwilyn <gwilyn@karmabunny.com.au>
2025-11-25 13:42:35 +1100

    Fix return typings for addSave().

--- src/Controllers/Admin/WordAdminController.php
+++ src/Controllers/Admin/WordAdminController.php
@@ -74,7 +74,7 @@ class WordAdminController extends HasCategoriesAdminController
      * Saves the provided POST data into a new record in the database
      *
      * @param int $item_id After saving, the new record id will be returned in this parameter
-     * @param bool True on success, false on failure
+     * @return bool True on success, false on failure
      */
     public function _addSave(&$item_id)
     {
